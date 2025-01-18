d0337bbc750309d3ac0a8d08cf63baf851538798
Tasya771 <tasyapardosi338@gmail.com>
2025-01-19 05:31:40 +0700

    Add files via upload

---
+++ stream-diabetes.py
@@ -0,0 +1,39 @@
+import pickle
+import streamlit as st
+
+# membaca model
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+
+#judul web
+st.title('Data Mining Prediksi Diabete')
+
+Pregnancies = st.text_input ('input nilai Pregnancies')
+
+Glucose = st.text_input ('input nilai Glucose')
+
+BloodPressure = st.text_input ('input nilai Blood Pressure')
+
+SkinThickness = st.text_input ('input nilai Skin Thickness')
+
+Insulin = st.text_input ('input nilai Insulin')
+
+BMI = st.text_input ('input nilai BMI')
+
+DiabetesPedigreeFunction = st.text_input ('input nilai Diabetes Pedigree Function')
+
+Age = st.text_input ('input nilai Age')
+
+# code untuk prediksi 
+diab_diagnosis = ''
+
+# membuat tombol untuk prediksi
+if st.button('Test Prediksi Diabetes'):
+    diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+
+    if(diab_prediction[0] == 1):
+        diab_diagnosis = 'Pasien Terkena Diabets'
+    else :
+        diab_diagnosis = 'pasien tidak terkena diabetes'
+    st.success(diab_diagnosis)
+
+
